eb295231588ad636581714728ac2209cd8ca4a51
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2025-08-13 21:55:07 -0400

    Update time_series_visualizer.py
    Commits through gitpod was denied. So manual changes on gitpod were made after code completion

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,32 +5,58 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
+print(df.head)
+print(len(df))
 
 # Clean data
-df = None
+df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
+print(df.head)
+print(len(df))
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(16, 6))
 
+    # Plot line chart
+    ax.plot(df.index, df['value'])
 
+    # Set labels and title
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
 
-
+    plt.tight_layout()
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
+
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df['year'] = df.index.year
+    df['month'] = df.index.month
+    df_bar = df.groupby(['year', 'month'])['value'].mean().reset_index(name='avg_value')
+    df_pivot = df_bar.pivot(index='year', columns='month', values='avg_value')
 
     # Draw bar plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    df_pivot.plot(kind='bar', ax=ax)
 
+    # Set labels and title on the same axes
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Average Page Views")
+    ax.set_title("Average Monthly Page Views per Year")
 
+    # Set legend with month names in correct order
+    ax.legend(title='Month', labels=[
+        'January', 'February', 'March', 'April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ])
 
-
+    plt.tight_layout()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -50,5 +76,5 @@ def draw_box_plot():
 
 
     # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+    # fig.savefig('box_plot.png')
+    # return fig
